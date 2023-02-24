6be3acc09655a535c09c03040cd40013e011c399
hhh3167253066 <3167253066@qq.com>
2023-02-24 23:41:38 +0800

    add: error handle and springboot

--- java_basic/src/basic/commonClass/Demo17Scanner02.java
+++ java_basic/src/basic/commonClass/Demo17Scanner02.java
@@ -22,6 +22,30 @@ public class Demo17Scanner02 {
 
         Scanner sc = methodReturn();
         int num = sc.nextInt();
+
+
+        //3.调用Scanner类的相关方法
+        System.out.println("请输入你的姓名：");
+        String name = sc.next();
+        System.out.println(name);
+
+        System.out.println("请输入你的芳龄：");
+        int age = sc.nextInt();
+        System.out.println(age);
+
+        System.out.println("请输入你的体重：");
+        double weight = sc.nextDouble();
+        System.out.println(weight);
+
+        System.out.println("你是否相中我了呢？(true/false)");
+        boolean isLove = sc.nextBoolean();
+        System.out.println(isLove);
+
+        //对于char型的获取，Scanner没有提供相关的方法。只能获取一个字符串
+        System.out.println("请输入你的性别：(男/女)");
+        String gender = sc.next();//"男"
+        char genderChar = gender.charAt(0);//获取索引为0位置上的字符
+        System.out.println(genderChar);
         System.out.println("输入的是：" + num);
     }
 
